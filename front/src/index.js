2b49f6a38c6ca82504defe644e5629afd4a80e29
Minwoo <minwoojung01@gmail.com>
2021-06-27 16:56:53 -0700

    Add redux and middlewares

--- front/src/index.js
+++ front/src/index.js
@@ -4,13 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import reduxThunk from 'redux-thunk';
+import reducer from './_reducers';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  reduxThunk
+)(createStore);
+
+// <Provider store={createStoreWithMiddleware(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={createStoreWithMiddleware(reducer)}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
